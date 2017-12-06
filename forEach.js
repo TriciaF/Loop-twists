1722a54b9fb85e143bc229a1989be8bc73b70d6d
TriciaF <TriciaF@thinkful.com>
2017-12-05 20:42:07 -0500

    added turtle steps filter, map and forEach functions

--- forEach.js
+++ forEach.js
@@ -1,18 +1,42 @@
 'use strict';
 
-const turtleSteps = [[0, 0], [0, 5], [-1, -3], [-3, 1], [2, -4], [3, 2]];
+let turtleSteps = [
+    [0, 0],
+    [0, 5],
+    [-1, -3],
+    [-3, 1],
+    [2, -4],
+    [3, 2]
+];
 
-turleSteps.filter(steps => steps[0] >= 0 && steps[1] >= 0);
-/*function filter(arr, fn) {
-  const newArray = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (fn(arr[i] >= 0)) {
-      newArray = arr.map(i);
-    }
-  }
-  console.log(newArray);
-  return newArray;
-} */
+//removes backward or right turtle steps from the array
+function filterTurtleSteps(arr) {
+    return arr.filter(steps => steps[0] >= 0 && steps[1] >= 0);
+}
 
-const value = filter(turtleSteps, function(steps)){
-return steps;
+//adds up turtle steps and returns array of turtle steps
+function howManyTurtleSteps(arr) {
+    let stepArray = [];
+    let i = 0;
+
+    arr.map(steps => {
+        stepArray[i] = steps[0] + steps[1];
+        i++;
+    });
+
+    return stepArray;
+}
+
+//prints out the array of turtle steps
+function printTurtleSteps(arr) {
+    arr.forEach(steps => {
+        console.log(`${steps} is the number of turtle steps taken`);
+    });
+}
+
+
+turtleSteps = filterTurtleSteps(turtleSteps);
+console.log(`Filtered array of Turtle Steps:  ${turtleSteps}`);
+turtleSteps = howManyTurtleSteps(turtleSteps);
+console.log(`How many turtle steps in the array: ${turtleSteps}`);
+printTurtleSteps(turtleSteps);
